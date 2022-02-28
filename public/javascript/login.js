c25c5a5cee70fb63155c096d275a305d9bcd1bad
Coder J <buildingguy@sbcglobal.net>
2022-02-27 18:20:40 -0800

    add login form and logic

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -25,4 +25,29 @@ async function signupFormHandler(event) {
     }
 }
 
+async function loginFormHandler(event) {
+  event.preventDefault();
+
+  const username = document.getElementById('u-name').value.trim();
+  const password = document.getElementById('password').value.trim();
+
+  if (email && password) {
+    const response = await fetch('/api/users/login', {
+      method: 'post',
+      body: JSON.stringify({
+        username,
+        password
+      }),
+      headers: { 'Content-Type': 'application/json' }
+    });
+
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+}
+
 document.getElementById('sign-up').addEventListener('submit', signupFormHandler);
+document.getElementById('.login').addEventListener('submit', loginFormHandler);

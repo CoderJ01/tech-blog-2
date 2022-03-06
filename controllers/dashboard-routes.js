99b172bf5075a12d45bee1b206e4331ae5a05cd5
Coder J <buildingguy@sbcglobal.net>
2022-03-05 20:34:01 -0800

    display logged-in user's blog posts

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,7 +3,30 @@ const sequelize = require('../config/connection');
 const { User, Blog, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('dashboard', { loggedIn: true });
-});
+    Blog.findAll({
+      where: {
+        // use the ID from the session
+        user_id: req.session.user_id
+      },
+      include: [
+        {
+          model: User,
+        }
+      ]
+    })
+      .then(dbBlogData => {
+        // serialize data before passing to template
+        const userBlog = dbBlogData.map(blog => blog.get({ plain: true }));
+        console.log(userBlog);
+        res.render('dashboard', { 
+            userBlog, 
+            loggedIn: true 
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;

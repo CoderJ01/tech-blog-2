6ce9274c894293ac9db360439bea1051b49cd916
Coder J <buildingguy@sbcglobal.net>
2022-03-06 21:11:10 -0800

    render edit blog page

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -3,6 +3,7 @@ const sequelize = require('../config/connection');
 const { User, Blog, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
+// retrieve all of a user's blog post to be retrieved in handlebars
 router.get('/', withAuth, (req, res) => {
     Blog.findAll({
       where: {
@@ -28,5 +29,43 @@ router.get('/', withAuth, (req, res) => {
         res.status(500).json(err);
       });
   });
+
+// allow user to edit blog posts
+router.get('/edit/:id', withAuth, (req, res) => {
+  const editBlogID = req.params.id;
+  Blog.findOne({
+    where: {
+      id: editBlogID
+    },
+    include: [
+      {
+        model: Comment
+      },
+      {
+        model: User
+      }
+    ]
+  })
+    .then(dbBlogData => {
+      if (dbBlogData) {
+        const editBlog = dbBlogData.get({ plain: true });
+        console.log(editBlog);
+        
+      res.render('edit-post', {
+          editBlog,
+          // loggedIn: true
+        });
+      } 
+      else {
+        console.log('404');
+        res.status(404).end();
+      }
+    })
+    .catch(err => {
+      console.log('500');
+      res.status(500).json(err);
+    });
+});
+  
 
 module.exports = router;

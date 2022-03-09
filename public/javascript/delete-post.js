c3fd6db211396823038d2d8e2fdfe33f9ce3c962
Coder J <buildingguy@sbcglobal.net>
2022-03-09 15:23:03 -0800

    allow user to delete posts

---
+++ public/javascript/delete-post.js
@@ -0,0 +1,24 @@
+// allow user to delete blogs
+async function deleteBlogFormHandler(event) {
+    event.preventDefault();
+
+    console.log('Delete button works');
+  
+    const id = window.location.toString().split('/')[
+      window.location.toString().split('/').length - 1
+    ];
+
+    const response = await fetch(`/api/blogs/${id}`, {
+      method: 'DELETE'
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } 
+    else {
+      alert(response.statusText);
+    }
+}
+  
+document.getElementById('b-delete').addEventListener('click', deleteBlogFormHandler);
+  

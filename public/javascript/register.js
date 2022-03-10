ae932894cc2ce732a62f35eed95098aeebdf0a81
Coder J <buildingguy@sbcglobal.net>
2022-03-09 17:09:45 -0800

    allow new user to sign up

---
+++ public/javascript/register.js
@@ -0,0 +1,32 @@
+async function signupFormHandler(event) {
+    event.preventDefault();
+  
+    const username = document.querySelector('input[name="sign-up-name"]').value.trim();
+    const email = document.querySelector('input[name="email"]').value.trim();
+    const password = document.querySelector('input[name="sign-up-password"]').value.trim();
+
+    console.log('This button works');
+    console.log(username);
+    console.log(email);
+    console.log(password);
+  
+    if (username && email && password) {
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({
+          username,
+          email,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert(response.statusText);
+      }
+    }
+}
+  
+document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);

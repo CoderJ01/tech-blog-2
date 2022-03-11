9f5e135e3ddebd6578f655975297d0ec08807350
Coder J <buildingguy@sbcglobal.net>
2022-03-11 15:08:23 -0800

    changes

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
-// const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -14,7 +13,6 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-// app.use(express.static('public'));
 
 const sess = {
   secret: 'Super secret secret',

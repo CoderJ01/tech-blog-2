84fec0b0ab990b0d13130555fa0bb2e7e6450c90
Coder J <buildingguy@sbcglobal.net>
2022-03-11 20:27:21 -0800

    test config set-up

--- db.config.js
+++ db.config.js
@@ -4,3 +4,8 @@ module.exports = {
     PASSWORD: "652fadb0@us",
     DB: "heroku_ea4c162762df38e"
 };
+
+// mysql --host=us-cdbr-iron-east-05.cleardb.net --user=be6b33011b5383 --password=652fadb0@us --reconnect heroku_ea4c162762df38e
+// https://www.bezkoder.com/deploy-node-js-app-heroku-cleardb-mysql/
+
+// https://dashboard.jawsdb.com/mysql/dashboard
